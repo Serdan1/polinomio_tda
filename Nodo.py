38394fe3df273c8394764b031629c6c0a05f1acd
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-04-02 10:29:51 +0000

    estructura

--- Nodo.py
+++ Nodo.py
@@ -1,18 +1,5 @@
 class Nodo:
-    """
-    Representa un nodo en una lista enlazada.
-    """
+    """Clase Nodo para representar un nodo en una lista enlazada."""
     def __init__(self, dato=None, siguiente=None):
-        """
-        Inicializa un nodo con un dato y un puntero al siguiente nodo.
-        :param dato: El dato almacenado en el nodo.
-        :param siguiente: Referencia al siguiente nodo en la lista.
-        """
         self.dato = dato
         self.siguiente = siguiente
-
-    def __repr__(self):
-        """
-        Representación en cadena del nodo.
-        """
-        return f"Nodo({self.dato})"
